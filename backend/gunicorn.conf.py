f4c3d4e4a63c54f3915223001280d70e3e431748
Rohan Pattnaik <pattnaikrohan77@gmail.com>
2026-03-12 09:21:38 +0530

    Add gunicorn and config for Azure

---
+++ backend/gunicorn.conf.py
@@ -0,0 +1,12 @@
+import multiprocessing
+
+max_requests = 1000
+max_requests_jitter = 50
+log_file = "-"
+bind = "0.0.0.0:8000"
+timeout = 600
+keepalive = 5
+graceful_timeout = 120
+num_cpus = multiprocessing.cpu_count()
+workers = (num_cpus * 2) + 1
+worker_class = "uvicorn.workers.UvicornWorker"
